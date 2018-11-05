17920efda1f97f9c38f34eda77560935b0d6a6c8
suswaram <git.suswaram@gmail.com>
2018-11-05 11:48:09 -0600

    Pom.xml : Reformatted file, added spring-boot-starter dependency and build plugin to enforce Java 1.8 during compile.
    LearnSpringBootApplication.java: Added custom "scanbasepackages" location
    
    compiler.xml - Changed heap size from 700 MB to 1024 MB

--- src/main/java/com/shvg/learn/springboot/application/LearnSpringBootApplication.java
+++ src/main/java/com/shvg/learn/springboot/application/LearnSpringBootApplication.java
@@ -1,9 +1,12 @@
 package com.shvg.learn.springboot.application;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = {"com.shvg.learn.springboot.common"})
+
 public class LearnSpringBootApplication {
 
 	public static void main(String[] args) {
