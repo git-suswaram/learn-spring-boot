7a208afd677168d07bb085f9118083618f437e39
suswaram <git.suswaram@gmail.com>
2018-11-05 10:02:03 -0600

    Base project generated by SPRING INITIALIZR for adding Web, DevTools, H2, Spring Integration, JPA and Actuator

---
+++ src/main/java/com/shvg/learn/springboot/application/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.shvg.learn.springboot.application;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(LearnSpringBootApplication.class);
+	}
+
+}
